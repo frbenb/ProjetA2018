96f483fe9af8b46bc4599e5468e7019b710f74f1
guillaumetousignant <moi_guillaume@hotmail.com>
2018-09-21 11:03:59 -0400

    new input

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ using namespace std;
 int main(){
 
     Mesh* mesh = new Mesh();
-    mesh->read_su2("./naca0012_129x129_1B_JAMESON.su2");
-    mesh->print();
+    mesh->read_tecplot("./naca0012_129x129_1B_JAMESON.x");
+    //mesh->print();
 
     return 0;
 }
