e50edb04ace2d28f377d2ca621935a7e278d0e69
frbenb <43252241+frbenb@users.noreply.github.com>
2018-09-20 20:27:13 -0400

    Add files via upload

---
+++ initialSystem.cpp
@@ -0,0 +1,108 @@
+#include "initialSystem.h"
+
+void InitialSystem::initialSyst()
+		:pi_(3.1416), gamma_(1.4), epsilon_(1.0e-28), dissip_(1), nbiter_(300), rungekutta_(5),
+		mach_(0.80), alpha_(1.25), reynolds_(11e6), tinf_(0), xref_(0.25), yref_(0), cmac_(1.0),
+		rhoInfini_(0), uInfini_(0), vInfini_(0), pInfini_(0), 
+		cltarget_(0), dcl_(0.001), rms0_(0){}
+
+float InitialSystem::getPi() const
+{
+	return pi_;
+}
+
+float InitialSystem::getGamma() const
+{
+	return gamma_;
+}
+
+float InitialSystem::getEpsilon() const
+{
+	return epsilon_;
+}
+
+unsigned int InitialSystem::getDissip() const
+{
+	return dissip_;
+}
+
+unsigned int InitialSystem::getNbiter() const
+{
+	return nbiter_;
+}
+
+unsigned int InitialSystem::getRungekutta() const
+{
+	return rungekutta_;
+}
+
+float InitialSystem::getMach() const
+{
+	return mach_;
+}
+
+float InitialSystem::getAlpha() const
+{
+	return alpha_;
+}
+
+float InitialSystem::getReynolds() const
+{
+	return reynolds_;
+}
+
+float InitialSystem::getTinf() const
+{
+	return tinf_;
+}
+
+float InitialSystem::getXref() const
+{
+	return xref_;
+}
+
+float InitialSystem::getYref() const
+{
+	return yref_;
+}
+
+float InitialSystem::getCmac() const
+{
+	return cmac_;
+}
+
+float InitialSystem::getRhoInfini() const
+{
+	return rhoInfini_;
+}
+
+float InitialSystem::getUInfini() const
+{
+	return uInfini_;
+}
+
+float InitialSystem::getVInfini() const
+{
+	return vInfini_;
+}
+
+float InitialSystem::getPInfini() const
+{
+	return pInfini_;
+}
+
+float InitialSystem::getClTargert() const
+{
+	return cltarget_;
+}
+
+float InitialSystem::getDcl() const
+{
+	return dcl_;
+}
+
+float InitialSystem::getrms0() const
+{
+	return rms0;
+}
+
---
+++ initialSystem.h
@@ -0,0 +1,54 @@
+#ifndef INITIALSYSTEM_H
+#define INITIALSYSTEM_H
+
+class InitialSystem
+{
+public:
+	void initialSystem(); //Constructor
+
+  //Get methods:
+  float getPi() const;
+  float getGamma() const;
+  float getEpsilon() const;
+  unsigned int getDissip() const;
+  unsigned int getNbiter() const;
+  unsigned int getRungekutta() const;
+
+  float getMach() const;
+  float getAlpha() const;
+  float getReynolds() const;
+  float getTinf() const;
+  float getXref() const;
+  float getYref() const;
+  float getCmac() const; //cmac?
+  float getRhoInfini() const;
+  float getUInfini() const;
+  float getVInfini() const;
+  float getPInfini() const;
+
+  float getClTargert() const; //cltarget?
+  float getDcl() const; //dcl?
+
+  float getrms0() const; //rms0?
+
+private:
+  float pi_, gamma_, epsilon_; 
+
+	/* constants from "input file" */
+  unsigned int dissip_, nbiter_, rungekutta_;
+
+  /* flow & geometry properties */
+	float mach_, alpha_, reynolds_; //from "input file"
+  float tinf_;
+	float xref_, yref_, cmac_; //from "input file"
+  float rhoInfini_, uInfini_, vInfini_, pInfini_; //rho_free, u_free, v_free, p_free
+  
+  /*constant cl run */
+  float cltarget_;
+  float dcl_; //from "input file"
+
+  /*convergence*/
+  float rms0_;
+}
+
+#endif
